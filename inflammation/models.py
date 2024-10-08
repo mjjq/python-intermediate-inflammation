bb7280bc25db698a1a685603f9562b8b6f94d572
Marcus Quantrill <mjjq500@york.ac.uk>
2024-10-08 13:24:56 +0100

    Add docstrings to functions

--- inflammation/models.py
+++ inflammation/models.py
@@ -41,15 +41,24 @@ def load_json(filename):
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array."""
+    """Calculate the daily mean of a 2D inflammation data array.
+    
+    :param data: Array of values to perform mean
+    """
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2D inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array.
+    
+    :param data: Array of values to perform max
+    """
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2D inflammation data array."""
+    """Calculate the daily min of a 2D inflammation data array.
+    
+    :param data: Array of values to perform min
+    """
     return np.min(data, axis=0)
