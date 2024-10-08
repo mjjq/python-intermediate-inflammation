a17e3822790f5242c9dd26cc9454da8c9e467dc5
Marcus Quantrill <mjjq500@york.ac.uk>
2024-10-08 14:19:21 +0100

    Add unit tests for min and max functions

--- tests/test_models.py
+++ tests/test_models.py
@@ -37,3 +37,48 @@ def test_load_from_json(tmpdir):
         temp_json_file.write('[{"observations":[1, 2, 3]},{"observations":[4, 5, 6]}]')
     result = load_json(example_path)
     npt.assert_array_equal(result, [[1, 2, 3], [4, 5, 6]])
+
+def test_daily_max_zeros():
+    """
+    Test that max function works for an array of zeros
+    """
+    from inflammation.models import daily_max
+
+    test_input = np.array([
+        [0, 0],
+        [0, 0],
+        [0, 0]
+    ])
+    test_result = np.array([0, 0])
+
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+
+def test_daily_max_integers():
+    """
+    Test that max function works for integers
+    """
+    from inflammation.models import daily_max
+
+    test_input = np.array([[1, 6],
+                           [3, 4],
+                           [5, 2]])
+    test_result = np.array([5, 6])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+
+def test_daily_min_integers():
+    """
+    Test that min function works for integers
+    """
+    from inflammation.models import daily_min
+
+    test_input = np.array([[1, 6],
+                           [3, 4],
+                           [5, 2]])
+    test_result = np.array([1, 2])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_min(test_input), test_result)
