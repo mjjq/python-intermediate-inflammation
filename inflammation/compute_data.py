5ff7b9d9254f85e4fa1283fb90c43b42a8e6e31c
Marcus Quantrill <mjjq500@york.ac.uk>
2024-10-21 14:35:23 +0100

    Regression test for compute_data

--- inflammation/compute_data.py
+++ inflammation/compute_data.py
@@ -28,4 +28,5 @@ def analyse_data(data_dir):
     graph_data = {
         'standard deviation by day': daily_standard_deviation,
     }
-    views.visualize(graph_data)
+    #views.visualize(graph_data)
+    return graph_data
---
+++ tests/test_compute_data.py
@@ -0,0 +1,63 @@
+from pathlib import Path
+import numpy as np
+
+
+def test_analyse_data():
+    from inflammation.compute_data import analyse_data
+
+    path = Path.cwd() / "./data"
+    result = analyse_data(path)
+
+    # TODO: add an assert for the value of result
+
+    assert "standard deviation by day" in result.keys()
+
+    result_array = result["standard deviation by day"]
+
+    np.testing.assert_array_almost_equal(
+        result_array,
+        np.array(
+            [
+                0.0,
+                0.22510286,
+                0.18157299,
+                0.1264423,
+                0.9495481,
+                0.27118211,
+                0.25104719,
+                0.22330897,
+                0.89680503,
+                0.21573875,
+                1.24235548,
+                0.63042094,
+                1.57511696,
+                2.18850242,
+                0.3729574,
+                0.69395538,
+                2.52365162,
+                0.3179312,
+                1.22850657,
+                1.63149639,
+                2.45861227,
+                1.55556052,
+                2.8214853,
+                0.92117578,
+                0.76176979,
+                2.18346188,
+                0.55368435,
+                1.78441632,
+                0.26549221,
+                1.43938417,
+                0.78959769,
+                0.64913879,
+                1.16078544,
+                0.42417995,
+                0.36019114,
+                0.80801707,
+                0.50323031,
+                0.47574665,
+                0.45197398,
+                0.22070227,
+            ]
+        ),
+    )
